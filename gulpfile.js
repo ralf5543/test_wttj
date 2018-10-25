5a912af07f0222b29b5d5187d8d1a6a791afcc84
unknown <contact@raphael-bigot.info>
2018-10-25 11:52:25 +0200

    added css and images to fill the widget

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('gulp-autoprefixer');
+var responsive = require('gulp-responsive');
 
 // ======================------------------ TASKS
 
@@ -18,6 +19,42 @@ gulp.task('css', function () {
 //Update css files each time a sass file is modified
 gulp.task('csswatch', function () {
   gulp.watch('./src/assets/css/**/*.scss', ['css']);
+});
+
+//Generates images at different sizes
+gulp.task('images', function () {
+
+  return gulp.src('./sources/*.{jpg,png}')
+      .pipe(responsive({
+
+        // Resize all images to three different sizes: 200, 400, 900 and 1280 pixels, eventually with their retina version (pixel ratio x2 or x3)
+        '*': [{
+          width: 420,
+          rename: { suffix: '-420px' }
+        }, {
+          width: 420 * 2,
+          rename: { suffix: '-420px_2x' }
+        }, {
+          width: 420 * 3,
+          rename: { suffix: '-420px_3x' }
+        }, {
+          // Compress, strip metadata, and rename original image
+          rename: { suffix: '-original' }
+        }],
+      }, {
+        // Global configuration for all images
+        // The output quality for JPEG, WebP and TIFF output formats
+        quality: 70,
+        // Use progressive (interlace) scan for JPEG and PNG output
+        progressive: true,
+        // Strip all metadata
+        withMetadata: false,
+        withoutEnlargement: true,
+        skipOnEnlargement: true, // that option copy original file with/without renaming
+        errorOnEnlargement: false,
+        crop: 'center'
+      }))
+      .pipe(gulp.dest('./src/assets/img'));
 });
 
 // Build task
